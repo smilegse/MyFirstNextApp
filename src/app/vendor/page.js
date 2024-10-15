c11be8478ba4f74c70a18d38de7ba9b6ae8a46c5
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2024-10-15 18:29:43 +0600

    create vendor commited

--- src/app/vendor/page.js
+++ src/app/vendor/page.js
@@ -5,15 +5,15 @@ import Menu from "@/components/Menu";
 const db = new PrismaClient();
 
 const Page = async() => {
-    const createVendor = await db.vendor.create({
-        data: {
-            BusinessEntityID:1690,
-            AccountNumber: "4321470000527795",
-            Name: "Md. Abu Bakar Siddiuqe",
-            CreditRating:1,
-            PurchasingWebServiceURL:null
-        }
-    })
+    // const createVendor = await db.vendor.create({
+    //     data: {
+    //         BusinessEntityID:1690,
+    //         AccountNumber: "4321470000527795",
+    //         Name: "Md. Abu Bakar Siddiuqe",
+    //         CreditRating:1,
+    //         PurchasingWebServiceURL:null
+    //     }
+    // })
 
     const vendors = await db.vendor.findMany({
         where: {
